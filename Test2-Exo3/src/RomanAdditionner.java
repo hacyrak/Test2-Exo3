7100e79d743dd4f7f3415941e0884439284f5848
a1007744 <a1007744@a-f004249.salsa.univ-amu.fr>
2019-02-28 17:08:06 +0100

    avancé

--- Test2-Exo3/src/RomanAdditionner.java
+++ Test2-Exo3/src/RomanAdditionner.java
@@ -1,4 +1,18 @@
 
 public class RomanAdditionner {
-
+	static public String compute(String a, String op, String b) {
+		String res = "";
+		RomanToNumeral roman = new RomanToNumeral();
+		NumeralToRoman numeral = new NumeralToRoman();
+		int chiffreA = roman.getNumeral(a);
+		int chiffreB = roman.getNumeral(b);
+		if (op.equals("+")) {
+			int add = chiffreA+chiffreB;
+			
+		} else if (op.equals("-")) {
+			
+		}
+		
+		return res;
+	}
 }
